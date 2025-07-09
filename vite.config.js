3baf24d6aa7d598449255852429151b190b35edb
Nyoni Safaris <monkeygenesafaris@gmail.com>
2025-07-09 21:50:26 +0100

    launch

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 
 export default defineConfig({
   base: '/munjiri-videos/',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        contact: path.resolve(__dirname, 'contact.html'),
+      }
+    }
   }
 })
