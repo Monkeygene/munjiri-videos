73713fa2d708838f21cfaee9519d61dea10e7fcc
Nyoni Safaris <monkeygenesafaris@gmail.com>
2025-07-13 08:53:09 +0100

    13-07

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,16 @@ export default {
      "./blog.html",
     "./contact.html",
     "./video-content-packages.html",
+    "./video-production-portfolio.html",
+    "./creative-video-productions.html",
+    "./charity-video-production.html",
+    "./nature-video-production.html",
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
       colors: {
-        linen: 'rgb(254, 252, 250)', // you can rename "linen" to anything
+        linen: 'rgb(254, 252, 250)',
       },
       fontFamily: {
         crimson: ['"Crimson Text"', 'serif'],
--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,10 @@ export default defineConfig({
         contact: path.resolve(__dirname, 'contact.html'),
         blog: path.resolve(__dirname, 'blog.html'),
         services: path.resolve(__dirname, 'video-content-packages.html'),
+        portfolio: path.resolve(__dirname, 'video-production-portfolio.html'),
+        creativevideoproductions: path.resolve(__dirname, 'creative-video-productions.html'),
+        charityvideoproduction: path.resolve(__dirname, 'charity-video-production.html'),
+        naturevideoproduction: path.resolve(__dirname, 'nature-video-production.html'),
       }
     }
   }
